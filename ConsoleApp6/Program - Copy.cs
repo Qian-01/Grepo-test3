6e476ce498e68728e694ecb33b20c2d50cfeacbf
Alice Lei (Beyondsoft Corporation) <v-qilei@microsoft.com>
2022-08-09 08:06:41 +0000

    11

--- ConsoleApp6/Program - Copy.cs
+++ ConsoleApp6/Program - Copy.cs
@@ -1,20 +1,30 @@
-﻿Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+//aaa
+//bbbb
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-//test2
-//aa
+Console.WriteLine("Hello, World!");//11
 //dd
+//ee
+//111
+//22
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
+//test1
+//test1
+//test1
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+//test1
+//test11111
+//aaaa
+//bb
+//111
+Console.WriteLine("Hello, World!");
+//test1
+//11
+//AAAA
+//Master1
