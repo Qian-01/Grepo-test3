5d0e82868cc42413b8c4350b3a04f770ba1279de
Alice Lei (Beyondsoft Corporation) <v-qilei@microsoft.com>
2022-08-25 08:15:04 +0000

    1

--- ConsoleApp6/Class2.cs
+++ ConsoleApp6/Class2.cs
@@ -4,7 +4,7 @@
     using System.Collections.Generic;
     using System.Linq;
     using System.Text;
-    using System.Threading.Tasks;//11
+    using System.Threading.Tasks;
 
     internal class Class2
     {
