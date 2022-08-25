915b789002e77c58218c059869f06a0d041d9684
Qian-01 <85333139+Qian-01@users.noreply.github.com>
2022-08-25 07:29:55 +0000

    Update Program.cs

--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -18,12 +18,6 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
-//test1
-//test11111
-//aaaa
-//bb
-//111
-//22
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
