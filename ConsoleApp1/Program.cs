594f8db1db7766995bdfa80da3f4b3c1e9f6a752
v-qilei <v-qilei@VSJADE-0407>
2022-10-26 23:47:30 -0700

    11

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,6 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System;
+
 Console.WriteLine("Hello, World!");
 //11
 //22
 Console.WriteLine("Hello, World!");
 //33
+onsole.WriteLine("Hello, World!");
