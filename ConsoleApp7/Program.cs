d3bc22b4c6b6247034acc82a357d5ffa7d22ab79
Alice Lei (Beyondsoft Corporation) <v-qilei@microsoft.com>
2023-06-06 06:48:03 +0000

    Revert "555"
    This reverts commit 3a6b52eca670590d5fce0458781e900625561482.

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -28,4 +28,3 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
