26dcfa655e2b96a1a907e6f56df4314e90f0bf14
Qian-01 <85333139+Qian-01@users.noreply.github.com>
2023-06-20 15:46:17 +0800

    Update Program.cs

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -1,4 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");//112233444aaaaaaaaaaaaaaaabbbb112222333
 Console.WriteLine("Hello, World!");
-//2223333aaaaabbb
+//111\
+//aaaaa
+//112233
+//1122
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+//11112222444555
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
