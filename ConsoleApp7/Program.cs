e867cea55ad550f7d833161402dd52ea8d7e4756
Alice Lei (Beyondsoft Corporation) <v-qilei@microsoft.com>
2023-06-20 07:45:11 +0000

    111

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -1,36 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");//112233444aaaaaaaaaaaaaaaabbbb112222333
 Console.WriteLine("Hello, World!");
-//111\
-//aaaaa
-//112233
-//1122
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-//11112222444555
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-//111
-Console.WriteLine("Hello, World!");
-//master1112222aaa3333444
-//222aaaa3333
-//1112222
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-//aaa111111aaa
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
