6cd568f9b021981a4dfc9b1a9bd144b2fe63238f
Alice Lei (Beyondsoft Corporation) <v-qilei@microsoft.com>
2023-07-27 08:26:45 +0000

    1

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -1,5 +1,39 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");//112233444aaaaaaaaaaaaaaaabbbb11222233344
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+//2223333aaaaabbb3333344455
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+//1112222aaaa333444bb555bbb
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+//2223333aaaaabbb3333344455
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+//1112222aaaa333444bb555bbb
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 //2223333aaaaabbb3333344455
 Console.WriteLine("Hello, World!");
