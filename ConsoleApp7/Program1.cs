ab59a4de818fbcc8148f18f18fdc84d6a53bb3b7
Alice Lei (Beyondsoft Corporation) <v-qilei@microsoft.com>
2023-08-04 06:57:28 +0000

    aaa333

--- ConsoleApp7/Program1.cs
+++ ConsoleApp7/Program1.cs
@@ -1,19 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");//112233444aaaaaaaaaaaaaaaabbbb11
 Console.WriteLine("Hello, World!");
-//111\
-//aaaaa
-//112233
-//1122
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-//11223344
-//1122aa
-//11aa
-Console.WriteLine("Hello, World!");//11111
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
