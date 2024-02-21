5d27549b72aa40fc6d7e430b9b494ae82a9b705b
Alice Lei (Beyondsoft Corporation) <v-qilei@microsoft.com>
2024-02-21 06:30:50 +0000

    aaa333

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -19,4 +19,5 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
 
--- ConsoleApp7/Program1.cs
+++ ConsoleApp7/Program1.cs
@@ -33,3 +33,4 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 //112233
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
