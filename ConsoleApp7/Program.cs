a87abd5fe2265bc70579b85e27e939dfd50694bd
Alice Lei (Beyondsoft Corporation) <v-qilei@microsoft.com>
2024-02-21 06:29:09 +0000

    11

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -16,16 +16,4 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 
-using System;
-
-namespace MyApp // Note: actual namespace depends on the project name.
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
 
