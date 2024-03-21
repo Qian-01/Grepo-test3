b00b04d3b0b8c13b9fb296fbd60ad8427b6b52f5
Alice Lei (Beyondsoft Corporation) <v-qilei@microsoft.com>
2024-03-21 06:59:12 +0000

    Removed `ConsoleApp7` namespace from `Class1.cs`
    Removed the entire `ConsoleApp7` namespace, including the `Class1` class and all its using directives (`System`, `System.Collections.Generic`, `System.Linq`, `System.Text`, `System.Threading.Tasks`) from the `Class1.cs` file.

--- ConsoleApp7/Class1.cs
+++ ConsoleApp7/Class1.cs
@@ -10,27 +10,4 @@ namespace ConsoleApp7
     {
     }
 }
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ConsoleApp7
-{
-    internal class Class1
-    {
-    }
-}
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ConsoleApp7
-{
-    internal class Class1
-    {
-    }
-}
