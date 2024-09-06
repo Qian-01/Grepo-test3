6c6bd687ef795dde2bfeff7bde4e8b56854ece1a
Alice Lei (Beyondsoft Corporation) <v-qilei@microsoft.com>
2024-09-06 07:04:09 +0000

    111

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -49,9 +49,3 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
-//1122222
-https://github.com/Alice-2024/Grepo-test3/blob/fd641c9d4840f8662229f8daa8dd36da8ddd99a4/ConsoleApp7/Program.cs?plain=1
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
