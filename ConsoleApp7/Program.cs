fdc839ff032943c818c59bfb48aaa5cbc9ff7f0d
Alice Lei (BEYONDSOFT CONSULTING INC) <v-qilei@microsoft.com>
2024-11-19 07:00:19 +0000

    Remove unused using directives from Program.cs
    Removed unnecessary using directives for System, System.Collections.Generic, System.Linq, System.Text, and multiple instances of System.Threading.Tasks. The repeated Console.WriteLine("Hello, World!"); lines are shown for context but remain unchanged.

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -18,10 +18,4 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Threading.Tasks;
-using System.Threading.Tasks;
+
