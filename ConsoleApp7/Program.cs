e01015e0f3e10cf27d05ccf0b1473f65812f72f5
v-qilei@microsoft.com <v-qilei@vqilei008>
2025-01-16 07:19:34 +0000

    "Remove duplicate Console.WriteLine in Program.cs"
      The line `Console.WriteLine("Hello, World!");` was removed from `Program.cs`, leaving a single instance of the line in the file.

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -30,4 +30,3 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
