4aee65ab382ded078387ff27d3aae906b4e7b5c6
Alice Lei (BEYONDSOFT CONSULTING INC) <v-qilei@microsoft.com>
2025-01-24 06:50:31 +0000

    Add multiple Hello World statements in Program.cs
    Enhanced the output by adding five additional
    Console.WriteLine("Hello, World!"); statements,
    bringing the total to seven in the file.

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -25,3 +25,7 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 //aaa111112222
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
